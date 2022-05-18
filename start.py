58fbb760e9d532bf27ee8b7eca56d0f1bd80c05c
Linar Sharifullin <48877848+LinarSharifullin@users.noreply>
2022-05-18 09:20:25 +0700

    Transition functions to class Account

--- start.py
+++ start.py
@@ -4,21 +4,31 @@ import json
 import steampy
 
 
-def read_maFile(name):
-    with open(f'maFiles/{name}', 'r') as file:
-        data = file.read()
-    return json.loads(data)
+class Account:
+    def __init__(self, file_name):
+        self._file_name = file_name
+        account_data = self._read_maFile()
+        self._shared_secret = account_data['shared_secret']
+        self._account_name = account_data['account_name']
+        self._identity_secret = account_data['identity_secret']
+        self._steam_id = account_data['Session']['SteamID']
+    
+    def _read_maFile(self):
+        with open(f'maFiles/{self._file_name}.maFile', 'r') as file:
+            data = file.read()
+        return json.loads(data)
 
-def get_accounts_data(files):
-    accounts_data = []
+
+def get_accounts(files):
+    accounts = []
     for file in files:
         if '.maFile' in file:
-            accounts_data.append(read_maFile(file))
-    return accounts_data
+            accounts.append(Account(file.strip('.maFile')))
+    return accounts
 
 def router():
     files_from_maFiles = os.listdir('maFiles')
-    accounts_data = get_accounts_data(files_from_maFiles)
+    accounts = get_accounts(files_from_maFiles)
 
 if __name__ == '__main__':
     router()
