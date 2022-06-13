4e4b120bd7cba4e1f8359ddddb355e9abbbebc5c
Linar Sharifullin <48877848+LinarSharifullin@users.noreply.github.com>
2022-06-13 21:31:33 +0700

    Refactoring auto_confirmations_mode

--- auto_confirmations_mode.py
+++ auto_confirmations_mode.py
@@ -7,111 +7,115 @@ from account import Account, check_account_sessions
 
 
 def auto_confirmations_router(accounts: List[Account]) -> None:
-    exit = False
     selected_accounts = []
-    while exit == False:
+    while True:
         if len(selected_accounts) == 0:
-            selected_accounts, exit = select_accounts(accounts)
-        else:
+            show_accounts(accounts)
+            user_response = input('Write: ').split()
+            if '0' in user_response:
+                return
+            elif user_response == []:
+                print('\nEmpty string received')
+                continue
+            try:
+                selected_accounts = process_accounts_response(user_response,
+                    accounts)
+            except (TypeError, IndexError) as exc:
+                print(f'\n{exc}')
+                continue
             check_account_sessions(selected_accounts)
-            exit, listings, trades = select_auto_confirmations_mode()
-            if exit == False and listings == True or trades == True:
+
+        show_confirmations_mode()
+        user_response = input('Write: ')
+        if user_response == '0':
+            return
+        try:
+            listings, trades = process_confirmations_mode_response(
+                user_response)
+        except TypeError as exc:
+            print(f'\n{exc}')
+            continue
+
+        if listings == True or trades == True:
+            try:
                 auto_confirmations(selected_accounts, listings, trades)
+            except KeyboardInterrupt:
                 return
 
-def select_accounts(accounts: list[Account]) -> List[Account]:
-    print()
-    print('Write the numeric of the desired account, '
+def show_accounts(accounts: List[Account]) -> None:
+    print('\nWrite the numeric of the desired account, '
         'you can several separated by a space:')
     print('0. Return to the main menu')
     print('1. Select all')
     for account_number in range(2, len(accounts)+2):
         print(f'{account_number}. {accounts[account_number-2].username}')
-    user_response = input('Write: ').split()
-    selected_accounts, exit = select_accounts_user_response_processing(
-        user_response, accounts)
-    return selected_accounts, exit
 
-def select_accounts_user_response_processing(user_response: List[str], 
-        accounts: List[Account]) -> Tuple[List[Account], bool]:
-    exit = False
+def process_accounts_response(user_response: list,
+        accounts: List[Account]) -> List[Account]:
     selected_accounts = []
-    if user_response == []:
-        print('Empty string received')
     for part in user_response:
         if part.isnumeric() == False:
-            print(f'{part} not numeric')
-            return [], False
-        elif int(part) == 0:
-            return [], True
+            raise TypeError(f'{part} not numeric')
         elif int(part) == 1:
-            return accounts, False
+            return accounts
         elif 1 < int(part) <= len(accounts)+1:
             account = accounts[int(part)-2]
             selected_accounts.append(account)
         else:
-            print(f'{part} not found')
-            return [], False
-    return selected_accounts, exit
+            raise IndexError(f'{part} not found')
+    return selected_accounts
 
-def select_auto_confirmations_mode() -> Tuple[bool]:
-    exit, listings, trades = False, False, False
-    print()
-    print('Write the numeric of the desired confirmations:')
+def show_confirmations_mode() -> None:
+    print('\nWrite the numeric of the desired confirmations:')
     print('0. Return to the main menu')
     print('1. Market transactions')
     print('2. Trades')
     print('3. Both (market transactions and trades)')
-    user_response = input('Write: ')
-    if '0' == user_response:
-        exit = True
-    elif '3' == user_response:
+
+def process_confirmations_mode_response(user_response: str) -> Tuple[bool]:
+    listings, trades = False, False
+    if '3' == user_response:
         listings, trades = True, True
     elif '1' == user_response:
         listings = True
     elif '2' == user_response:
         trades = True
     else:
-        print(f'{user_response} - invalid response')
-    return exit, listings, trades
+        raise TypeError(f'{user_response} not found')
+    return listings, trades
 
 def auto_confirmations(accounts: List[Account], listings: bool,
         trades: bool) -> None:
-    print()
-    print('Entered auto-confirmation mode, press CTRL + C to exit in main menu')
+    print('\nEntered auto-confirmation mode, press CTRL + C',
+        'to exit in main menu')
     delay = 30
-    try:
-        while True:
-            for account in accounts:
-                try:
-                    confirmations = account.steam_client.confirmations\
-                        .get_confirmations()
-                except AttributeError:
-                    print('An error occurred while receiving',
-                        'confirmations: AttributeError')
-                except IndexError:
-                    print('An error occurred while receiving',
-                        'confirmations: IndexError')
-                process_confirmations(confirmations, listings, trades, account)
-                time.sleep(delay)
-    except KeyboardInterrupt:
-        return
+    while True:
+        for account in accounts:
+            try:
+                confirmations = account.steam_client.confirmations\
+                    .get_confirmations()
+                if len(confirmations) > 0:
+                    process_confirmations(confirmations, listings,
+                        trades, account)
+                else:
+                    print(f'No confirmations from account {account.username}')
+            except (AttributeError, IndexError) as exc:
+                print('An error occurred while receiving',
+                    f'confirmations: {exc}')
+            time.sleep(delay)
 
 def process_confirmations(confirmations: List[Confirmation], listings: bool,
         trades: bool, account: Account) -> None:
-    if len(confirmations) == 0:
-        print(f'No confirmations from account {account.username}')
-        return
-    confirmations_for_allow = []
     print(f'Received {len(confirmations)} confirmations from', 
         f'account {account.username}')
+    confirmations_for_allow = []
+    create_listing_value = ConfirmationType.create_listing.value
+    trades_value = ConfirmationType.trade.value
     for confirmation in confirmations:
-        if listings == True\
-                and ConfirmationType.create_listing.value\
-                == confirmation.conf_type:
+        conf_type = confirmation.conf_type
+        if listings == True and create_listing_value == conf_type:
             confirmations_for_allow.append(confirmation)
-        if trades == True\
-                and ConfirmationType.trade.value == confirmation.conf_type:
+        if trades == True and trades_value == conf_type:
             confirmations_for_allow.append(confirmation)
     if len(confirmations_for_allow) <= 0:
         print('No suitable confirmations')
